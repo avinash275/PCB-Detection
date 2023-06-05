286cae1927d3df0a66b123e7f59d0bc309da7cc8
avinash275 <104912103+avinash275@users.noreply.github.com>
2023-06-05 09:51:39 +0530

    Update main.py
    after accepting file from user from its local directory

--- main.py
+++ main.py
@@ -4,32 +4,24 @@ from matplotlib import pyplot as plt
 import cv2
 import numpy as np
 from PIL import Image
+import os
+import tempfile
 st.set_page_config(page_title="PCB Detection",page_icon=":tada:",layout="wide")
-
 st.title("PCB Detection")
 import requests
 
-def download_image(url, save_path):
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(save_path, 'wb') as file:
-            file.write(response.content)
-        print("Image downloaded successfully.")
-    else:
-        print("Failed to download image. Status code:", response.status_code)
-
-
-image_url = "http://127.0.0.1:8800/image"
-save_file_path = "input.jpg"
-download_image(image_url, save_file_path)
-
-def imshow(title = "", image = None, size = 10):
-    w, h = image.shape[0], image.shape[1]
-    aspectRatio = w / h
-    plt.figure(figsize=(size * aspectRatio, size))
-    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    plt.title(title)
-    plt.show()
+#for taking image from api
+# def download_image(url, save_path):
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         with open(save_path, 'wb') as file:
+#             file.write(response.content)
+#         print("Image downloaded successfully.")
+#     else:
+#         print("Failed to download image. Status code:", response.status_code)
+# image_url = "http://127.0.0.1:8800/image"
+# save_file_path = "input.jpg"
+# download_image(image_url, save_file_path)
 
 def getContours(imageFile):
     grayScaleImage2 = cv2.cvtColor(imageFile, cv2.COLOR_BGR2GRAY)
@@ -49,66 +41,76 @@ def difference(val1, val2):
     return numDiffContours
 
 sourceImage = cv2.imread('standard.jpg')
-imshow('Source Image', sourceImage, 5)
-print('Source Image RES: ', sourceImage.shape[0],'x',sourceImage.shape[1])
+st.subheader("PCB Image Uploader")
+
+inputImage = st.file_uploader("Choose PCB Image...", type=["jpg", "jpeg"])
+if inputImage is not None:
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    temp_file.write(inputImage.read())
+    temp_file.close()
+    image_path = temp_file.name
+    xinputi= cv2.imread(image_path)
+    with st.container():
+        st.write("---")
+        source_column,output_column=st.columns(2)
+        with source_column:
+            st.image(sourceImage,width=500, caption='source Image')
+        with output_column:
+            st.image(inputImage,width=500, caption='Input Image')
+    difference(getContours(sourceImage), getContours(xinputi))
+    height, width, channels = sourceImage.shape
+
+    quadrant_width = width // 2
+    quadrant_height = height // 2
+
+    sQ1 = sourceImage[:quadrant_height, :quadrant_width, :]
+    sQ2 = sourceImage[:quadrant_height, quadrant_width:, :]
+    sQ3 = sourceImage[quadrant_height:, :quadrant_width, :]
+    sQ4 = sourceImage[quadrant_height:, quadrant_width:, :]
 
 
-st.image(sourceImage,width=500, caption='source Image')
+    height, width, channels = xinputi.shape
 
-inputImage = cv2.imread('input.jpg')
-imshow('Input Image', inputImage, 5)
-print('Input Image RES: ', inputImage.shape[0],'x',inputImage.shape[1])
+    quadrant_width = width // 2
+    quadrant_height = height // 2
 
-st.image(inputImage,width=500, caption='Input Image')
-difference(getContours(sourceImage), getContours(inputImage))
+    iQ1 = xinputi[:quadrant_height, :quadrant_width, :]
+    iQ2 = xinputi[:quadrant_height, quadrant_width:, :]
+    iQ3 = xinputi[quadrant_height:, :quadrant_width, :]
+    iQ4 = xinputi[quadrant_height:, quadrant_width:, :]
 
-height, width, channels = sourceImage.shape
-
-quadrant_width = width // 2
-quadrant_height = height // 2
-
-sQ1 = sourceImage[:quadrant_height, :quadrant_width, :]
-sQ2 = sourceImage[:quadrant_height, quadrant_width:, :]
-sQ3 = sourceImage[quadrant_height:, :quadrant_width, :]
-sQ4 = sourceImage[quadrant_height:, quadrant_width:, :]
-
-
-height, width, channels = inputImage.shape
-
-quadrant_width = width // 2
-quadrant_height = height // 2
-
-iQ1 = inputImage[:quadrant_height, :quadrant_width, :]
-iQ2 = inputImage[:quadrant_height, quadrant_width:, :]
-iQ3 = inputImage[quadrant_height:, :quadrant_width, :]
-iQ4 = inputImage[quadrant_height:, quadrant_width:, :]
-
-sourceImageArray = [sQ1, sQ2, sQ3, sQ4]
-inputImageArray = [iQ1, iQ2, iQ3, iQ4]
-
-effectedAreaList = []
-for i in range(len(sourceImageArray)):
-    diff = difference(getContours(sourceImageArray[i]), getContours(inputImageArray[i]))
-    if diff != 0:
-        effectedAreaList.append(1)
+    sourceImageArray = [sQ1, sQ2, sQ3, sQ4]
+    inputImageArray = [iQ1, iQ2, iQ3, iQ4]
+    st.write("---")
+    effectedAreaList = []
+    for i in range(len(sourceImageArray)):
+        diff = difference(getContours(sourceImageArray[i]), getContours(inputImageArray[i]))
+        if diff != 0:
+            effectedAreaList.append(1)
+        else:
+            effectedAreaList.append(0) 
+    if effectedAreaList==[0,0,0,0]:
+        st.subheader("PCB is not Defected")
     else:
-        effectedAreaList.append(0) 
-
-Quadrant1=[1,0,0,0]
-Quadrant2=[0,1,0,0]
-Quadrant3=[0,0,1,0]
-Quadrant4=[0,0,0,1]
-if effectedAreaList==Quadrant1:
-    st.subheader("Defect detected in Quadrant 1")
-    st.image(iQ1,width=500, caption='Input Quadrant 1')
-elif effectedAreaList==Quadrant2:
-    st.subheader("Defect detected in Quadrant 2")
-    st.image(iQ2,width=500, caption='Input Quadrant 2')
-elif effectedAreaList==Quadrant3:
-    st.subheader("Defect detected in Quadrant 3")
-    st.image(iQ3,width=500, caption='Input Quadrant 3')
-elif effectedAreaList==Quadrant4:
-    st.subheader("Defect detected in Quadrant 4")
-    st.image(iQ4,width=500, caption='Input Quadrant 4')
-else:
-    st.write("PCB is not Defected")
+        for i in range(0,len(effectedAreaList)):
+            if i==0:
+                iQ=iQ1
+                sQ=sQ1
+            elif i==1:
+                iQ=iQ2
+                sQ=sQ2
+            elif i==2:
+                iQ=iQ3
+                sQ=sQ3
+            else:
+                iQ=iQ4
+                sQ=sQ4
+            if effectedAreaList[i]==1:
+                st.subheader(f"Defect detected in Quadrant {i+1}")
+                with st.container():
+                    st.write("---")
+                    source_column,output_column=st.columns(2)
+                    with source_column:
+                        st.image(sQ,width=500, caption=f'Source Quadrant {i+1}')
+                    with output_column:
+                        st.image(iQ,width=500, caption=f'Input Quadrant {i+1}')
